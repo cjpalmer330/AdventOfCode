7ceeb8da315efae511e7d70d5febdf96487f3011
cjpalmer330 <cjpalmer330@gmail.com>
2023-12-18 10:22:37 -0600

    day 2 - part 2
    added  the power and power sum functions

--- main.py
+++ main.py
@@ -2,52 +2,70 @@ import fileinput
 
 maxRed, maxBlue, maxGreen = 12, 14, 13
 validIDs = []
-for line in fileinput.input(files='adventDay2Input.txt', encoding='utf-8'):
+powerSum = 0
+for bagLine in fileinput.input(files='adventDay2Input.txt', encoding='utf-8'):
 
     invalid = False
     # getting game ID
-    splitLine = line.split(':')
+    splitLine = bagLine.split(':')
     idString = splitLine[0]
     gameID = idString[5:]
 
     # each gameSplit is one bag, three rounds per bag
-    line.strip()
-    gameSplitLine = line[8:].split(';')
+    bagLine.strip()
+    gameSplitLine = bagLine[8:].split(';')
 
     redCount, blueCount, greenCount = 0, 0, 0
-    for round in gameSplitLine:
+    redMax, greenMax, blueMax = 0, 0, 0
+    for pullRound in gameSplitLine:
 
         # get each colors value
-        colorSplit = round.split(',')
+        colorSplit = pullRound.split(',')
         for pull in colorSplit:
             # red count
             if pull.__contains__("red"):
                 f = filter(str.isdecimal, pull)
                 pull = "".join(f)
                 redCount = int(pull)
+
+                if redCount > redMax:
+                    redMax = redCount
+
             # green count
             elif pull.__contains__("green"):
                 f = filter(str.isdecimal, pull)
                 pull = "".join(f)
                 greenCount = int(pull)
+
+                if greenCount > greenMax:
+                    greenMax = greenCount
+
             # blue count
             elif pull.__contains__("blue"):
                 f = filter(str.isdecimal, pull)
                 pull = "".join(f)
                 blueCount = int(pull)
 
-        # the game is not valid and we move to the next game id
+                if blueCount > blueMax:
+                    blueMax = blueCount
+
+        # the game is not valid, and we move to the next game id
         if redCount > maxRed or blueCount > maxBlue or greenCount > maxGreen:
             print("invalid ID", gameID)
             invalid = True
-            break
+
+    # getting power and power sum
+    power = redMax * greenMax * blueMax
+    powerSum += power
 
     # made it through without finding faults, add to list
     if not invalid:
+
         validIDs.append(gameID)
-        print("valid ID --: ", gameID)
+        print("valid ID --: ", gameID, "power: ", power)
 IDSum = 0
 for ID in validIDs:
     IDSum += int(ID)
 
 print(IDSum)
+print("powerSum: ", powerSum)
