056a90789e6b43afeb2b7d15021805bf51744d35
cjpalmer330 <cjpalmer330@gmail.com>
2023-12-18 10:03:26 -0600

    Day 2 Part 1

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
-
 import fileinput
 
-totalSum = 0
-for line in fileinput.input(files='adventDay1Input.txt', encoding='utf-8'):
+maxRed, maxBlue, maxGreen = 12, 14, 13
+validIDs = []
+for line in fileinput.input(files='adventDay2Input.txt', encoding='utf-8'):
 
-    # getting line and removing white space
-    line = ''.join(line.split())
-    tempString = ""
+    invalid = False
+    # getting game ID
+    splitLine = line.split(':')
+    idString = splitLine[0]
+    gameID = idString[5:]
 
-    #each character, if digit add to sum
-    for character in line:
+    # each gameSplit is one bag, three rounds per bag
+    line.strip()
+    gameSplitLine = line[8:].split(';')
 
-        if character.isdigit() and (character != ""):
-            tempString += character
+    redCount, blueCount, greenCount = 0, 0, 0
+    for round in gameSplitLine:
 
-    # getting first and last digit
-    firstChar = tempString[0]
-    tempString = firstChar + tempString[tempString.__len__() - 1]
-    totalSum += int(tempString)
+        # get each colors value
+        colorSplit = round.split(',')
+        for pull in colorSplit:
+            # red count
+            if pull.__contains__("red"):
+                f = filter(str.isdecimal, pull)
+                pull = "".join(f)
+                redCount = int(pull)
+            # green count
+            elif pull.__contains__("green"):
+                f = filter(str.isdecimal, pull)
+                pull = "".join(f)
+                greenCount = int(pull)
+            # blue count
+            elif pull.__contains__("blue"):
+                f = filter(str.isdecimal, pull)
+                pull = "".join(f)
+                blueCount = int(pull)
 
-print(totalSum)
+        # the game is not valid and we move to the next game id
+        if redCount > maxRed or blueCount > maxBlue or greenCount > maxGreen:
+            print("invalid ID", gameID)
+            invalid = True
+            break
+
+    # made it through without finding faults, add to list
+    if not invalid:
+        validIDs.append(gameID)
+        print("valid ID --: ", gameID)
+IDSum = 0
+for ID in validIDs:
+    IDSum += int(ID)
+
+print(IDSum)
